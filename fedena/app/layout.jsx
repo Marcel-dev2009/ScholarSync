eaddbd7de9be2dbc2b9446a6780aebd86efb96d0
Marcel <izuobasomtochukwu@gmail.com>
2025-08-07 03:10:39 +0100

    updated MetaData

--- fedena/app/layout.jsx
+++ fedena/app/layout.jsx
@@ -3,8 +3,8 @@ import { Provider } from "./provider";
 import "./globals.css";
 export const metadata = {
   title:{
-   default: 'Skolo-Management System' ,
-   template: '%s | Skolo'
+   default: 'ScholarSync' ,
+   template: '%s | ScholarSync'
   },
   description: `A Full Stack School Management System To automate the administrative tasks of school
   Built In partnership with Command Day Seconadary School
